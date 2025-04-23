e8c08b69921d8215e2d9dce6057cb185440a46ff
Pratik Bhanushali <141171336+Pratik-0511@users.noreply.github.com>
2025-04-23 17:55:18 +0530

    Create updated-script.js

---
+++ updated-script.js
@@ -0,0 +1,29 @@
+console.log("rannnnnn");
+
+
+console.log("2nd");
+
+fetch("https://apiv2.shiprocket.in/v1/external/auth/login", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    email: "info@neostretch.in",
+    password: "|~u89U0JYVz@"
+  })
+})
+  .then(response => response.json())
+  .then(data => {
+    if (data.token) {
+      console.log("Shiprocket Login Succeess");
+      console.log("Token:", data.token);
+
+      // Now you can use this token for other API calls
+    } else {
+      console.error("Login failed:", data);
+    }
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
